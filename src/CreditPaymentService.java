4a5438ba927612b32d962f078bcdf6c03951c6a8
Oleg <ovt1966@gmail.com>
2023-04-07 17:49:54 +0300

    Change files

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,8 @@
 public class CreditPaymentService {
-    double annuitet;
 
     double calculate(int sumKredit, int numberOffMonth, double rate) {
         double baseOffDegree = (1 + rate / 1200);
-        annuitet = sumKredit * rate / 1200 * Math.pow(baseOffDegree, numberOffMonth) /
+       double annuitet = sumKredit * rate / 1200 * Math.pow(baseOffDegree, numberOffMonth) /
                 (Math.pow(baseOffDegree, numberOffMonth) - 1);
         return (int) annuitet;
     }
