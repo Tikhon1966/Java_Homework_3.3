b13d1d4b08372163cbf4ef0bf436e3ef8a3c5530
Oleg <ovt1966@gmail.com>
2023-04-07 20:05:40 +0300

    Format cod

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
 
     double calculate(int sumKredit, int numberOffMonth, double rate) {
         double baseOffDegree = (1 + rate / 1200);
-       double annuitet = sumKredit * rate / 1200 * Math.pow(baseOffDegree, numberOffMonth) /
+        double annuitet = sumKredit * rate / 1200 * Math.pow(baseOffDegree, numberOffMonth) /
                 (Math.pow(baseOffDegree, numberOffMonth) - 1);
         return (int) annuitet;
     }
